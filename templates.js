debb3ab37016994d1369cca2958a17b460cd5570
cassdowns <hello@cassandradowns.com>
2025-12-20 15:34:26 +0000

    yay!

--- templates.js
+++ templates.js
@@ -11,29 +11,18 @@ const menuTemplate = `
 
 // Footer template
 const footerTemplate = `
-<footer class="footer">
-  <div class="socials">
-    <div id="insta">
-      <a href="https://instagram.com/cassandradowns" target="_blank">
-        <img src="instagram-icon.png" alt="Instagram" />
-      </a>
+<div class="footer">
+    <div class="socials">
+        <div id="insta">
+            <a href="https://instagram.com/cassandradowns" target="_blank">
+                <img src="instagram-icon.png" alt="Instagram" />
+            </a>
+        </div>
+        <div id="copyright">
+            &copy; Cassandra Downs <span id="year"></span>
+        </div>
     </div>
-    <div id="copyright">
-      &copy; Cassandra Downs <span id="year"></span>
-    </div>
-  </div>
-</footer>
-<style>
-  .footer { width: 100%; display: table; padding: 10px 0; text-align: left; border-top: 1px solid #ccc; }
-  .socials { display: table-row; vertical-align: middle; }
-  #insta, #copyright { display: table-cell; vertical-align: middle; }
-  #insta { padding-right: 10px; }
-  #insta img { width: 24px; height: 24px; display: block; }
-  @media (max-width: 768px) {
-    .socials { display: flex; align-items: center; gap: 10px; }
-    #insta, #copyright { display: inline-block; vertical-align: middle; }
-  }
-</style>
+</div>
 `;
 
 // Function to inject HTML into placeholder

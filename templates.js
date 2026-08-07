d332716d8b7221ab3a5901b4a3b61ec295300dc8
cassdowns <hello@cassandradowns.com>
2026-08-07 17:23:14 +0100

    attempt at a caption

--- templates.js
+++ templates.js
@@ -33,7 +33,8 @@ const works = [
             'For over 100 years my family has been disconnected from Country, Community, and Culture. The impact of colonisation on our identity was so deeply ingrained that I always felt too afraid to reach out - that I was a fraud for not being able to answer "who\'s your mob, where are you from?"',
             'This Gurrborra (Koala) is a reflection of our journey. It matured under intense stress, leaving it scarred from what it went through - but most importantly, unbroken. Once removed from the fire, it was lovingly repaired and coated in the sap of Garrong, the grandparent plant, to protect it for what\'s to come.',
             'Like the Gurrborra, our family is slow to reach our destination, but every day I feel our ancestors pushing me to regain what was kept from us. It doesn\'t matter how long we were lost, as long as we eventually come home.',
-            'All components were foraged and crafted on Wurundjeri Country, and fired on Boon Wurrung Country - with gratitude.'
+            'All components were foraged and crafted on Wurundjeri Country, and fired on Boon Wurrung Country - with gratitude.',
+            '<p class="formNote">Original sculpture in a private collection in Melbourne, AUS. Limited edition prints available as part of the <a href="/prints/ndos.html>\'Pre-Fire\'</a> series.'
         ],
         images: [
             '/img/koala-front.jpg',

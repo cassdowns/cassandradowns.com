cd4ef424abea2c03eaa80a8c73dfc85fdccd93a3
cassdowns <hello@cassandradowns.com>
2026-08-17 20:08:18 +0100

    Update journals-data.js

--- js/journals-data.js
+++ js/journals-data.js
@@ -11,6 +11,17 @@ const journals = [
         contentFile: '/journals/relearning.md',
         url: '/journals/relearning-the-soil.html'
     },
+
+    {
+        slug: 'relearning-the-soil',
+        title: 'Relearning the Soil 2',
+        date: '2026-01-01',
+        dateLabel: 'January 2026',
+        excerpt: 'In 2021 I dug a hole in my backyard. It was lockdown, I had nowhere else to go, and I\'d noticed the soil wasn\'t very interested in growing vegetables - but maybe I could still make use of it.',
+        image: '/img/journals/wildclay-hole.png',
+        contentFile: '/journals/relearning.md',
+        url: '/journals/relearning-the-soil.html'
+    },
 ];
 
 /* Unpublished drafts
